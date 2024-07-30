2f3fd940e9782c4f92ccfc84b243c30772f2fe14
shilangyazhou <shilyz726@163.com>
2024-07-30 11:45:36 +0800

    Add files via upload

--- text.py
+++ text.py
@@ -1,13 +1,12 @@
-
+# 读取文本数据
 with open('words_space.txt','r',encoding='utf-8') as f:
-        words_list = f.read().split('\n')
-print(words_list)
-print("-"*100)
-
+    words_list = f.read().split('\n')[:5]
+print('-'*100)
+#处理单词
 for i in range(len(words_list)):
-    words_list[i]=words_list[i].split('#')
+    words_list[i] = words_list[i].split('#')
 print(words_list)
-print("-"*100)
+print('-'*100)
 
 word_dict = {}
 for w in words_list:
